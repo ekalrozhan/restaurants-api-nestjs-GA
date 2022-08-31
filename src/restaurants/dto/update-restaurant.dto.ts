af6c28af8282457800b9c36acdb02e079550b3f7
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-08-31 22:09:59 +0800

    adding validation rules in updateDto

--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,11 +1,29 @@
+import { IsString, IsEmail, IsPhoneNumber, IsEnum, IsOptional } from "class-validator"
 import { Category } from "../schemas/restaurant.schema"
 
 export class UpdateRestaurantDto{
    
+    @IsString()
+    @IsOptional()
     readonly name: string
+
+    @IsString()
+    @IsOptional()
     readonly description: string
+
+    @IsEmail({}, {message: 'Please enter correct email address'})
+    @IsOptional()
     readonly email: string
+
+    @IsPhoneNumber('MY')
+    @IsOptional()
     readonly phoneNo: Number
+
+    @IsString()
+    @IsOptional()
     readonly address: string
+
+    @IsEnum(Category, {message: 'Please enter correct category'})
+    @IsOptional()
     readonly category: Category
 }
