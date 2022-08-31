47a1c9653ac641c250f97b1c10fc779ea2d2a2d5
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-08-31 21:44:15 +0800

    pagination

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -14,6 +14,10 @@ export class RestaurantsService{
     // get all restaurants => GET /restaurants
        async findAll(query: Query) : Promise<Restaurant[]>{
 
+        const resPerPage = 2
+        const currentPage = Number(query.page) || 1
+        const skip = resPerPage * (currentPage -1)
+
           const keyword = query.keyword ? {
             name: {
               $regex: query.keyword,
@@ -21,7 +25,7 @@ export class RestaurantsService{
             }
           }: {}
 
-            const restaurants = await this.restaurantModel.find({...keyword})
+            const restaurants = await this.restaurantModel.find({...keyword}).limit(resPerPage).skip(skip)
             return restaurants
        }
 
