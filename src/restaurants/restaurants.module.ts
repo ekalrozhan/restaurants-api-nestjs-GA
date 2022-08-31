0e7158095b17fbf4e74f7a4087224c7fb4a12210
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-08-31 16:06:46 +0800

    restaurant schema

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
 
+import { RestaurantSchema } from './schemas/restaurant.schema';
+
 @Module({
+  imports: [MongooseModule.forFeature([
+    {name: 'Restaurant', schema: RestaurantSchema}
+  ])],
   controllers: [RestaurantsController],
   providers: [RestaurantsService]
 })
